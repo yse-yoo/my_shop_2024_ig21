47c6fe172e4d25e16cd05a6ec41d1796787a5d5b
yse-yoo <youhei_yoshikawa@yse-net.com>
2024-04-22 16:07:52 +0900

    update

--- regist/confirm.php
+++ regist/confirm.php
@@ -49,8 +49,12 @@ function validate($posts)
     if (empty($posts['email'])) {
         $errors['email'] = 'メールアドレスを入力してください';
     }
+
+    $pattern = '/^(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{6,12}$/';
     if (empty($posts['password'])) {
         $errors['password'] = 'パスワードを入力してください';
+    } elseif (!preg_match($pattern, $posts['password'])) {
+        $errors['password'] = 'パスワードは6文字以上12文字以内の半角英数で入力してください。（大文字、文字含む）';
     }
     return $errors;
 }
