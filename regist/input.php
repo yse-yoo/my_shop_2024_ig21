62b5ea3d866d56fcb75026ac575c7c5cb05e68d6
yse-yoo <youhei_yoshikawa@yse-net.com>
2024-04-22 12:01:48 +0900

    update

--- regist/input.php
+++ regist/input.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 // セッション開始
 session_start();
 
@@ -15,32 +15,35 @@ if (!empty($_SESSION['my_shop']['regist'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Shop</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 
 <body>
-    <h2>会員登録</h2>
-    <!-- form request confirm.php にデータをPOST送信-->
-    <form action="confirm.php" method="post">
-        <div>
-            <label for="">名前</label>
-            <input type="text" name="name" value="<?= @$regist['name'] ?>">
-        </div>
+    <main class="container">
+        <h2>会員登録</h2>
+        <!-- form request confirm.php にデータをPOST送信-->
+        <form action="confirm.php" method="post">
+            <div>
+                <label class="form-label" for="">名前</label>
+                <input class="form-control" type="text" name="name" value="<?= @$regist['name'] ?>">
+            </div>
 
-        <div>
-            <label for="">Email</label>
-            <input type="email" name="email" value="<?= @$regist['email'] ?>">
-        </div>
+            <div>
+                <label class="form-label" for="">Email</label>
+                <input class="form-control" type="email" name="email" value="<?= @$regist['email'] ?>">
+            </div>
 
-        <div>
-            <label for="">パスワード</label>
-            <input type="password" name="password">
-        </div>
+            <div>
+                <label class="form-label" for="">パスワード</label>
+                <input class="form-control" type="password" name="password">
+            </div>
 
-        <div>
-            <button>次へ</button>
-            <a href="">ログイン画面へ</a>
-        </div>
-    </form>
+            <div class="mt-3 text-center">
+                <button class="btn btn-primary">次へ</button>
+                <a class="btn btn-outline-primary" href="">ログイン画面へ</a>
+            </div>
+        </form>
+    </main>
 </body>
 
 </html>
