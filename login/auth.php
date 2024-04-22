5d5e33ceee11d25b4e06e4daea645783f411af37
yse-yoo <youhei_yoshikawa@yse-net.com>
2024-04-22 14:35:24 +0900

    update

--- login/auth.php
+++ login/auth.php
@@ -1,25 +1,10 @@
 <?php
 //設定ファイル「env.php」読み込み
 require_once '../env.php';
+require_once '../lib/DB.php';
 
-//TODO: データベース接続処理
-$db_connection = DB_CONNECTION;
-$db_name = DB_DATABASE;
-$db_host = DB_HOST;
-$db_port = DB_PORT;
-$db_user = DB_USERNAME;
-$db_password = DB_PASSWORD;
-
-$dsn = "{$db_connection}:dbname={$db_name};host={$db_host};charset=utf8;port={$db_port}";
-try {
-    $pdo = new PDO($dsn, $db_user, $db_password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-} catch (PDOException $e) {
-    echo "接続失敗: " . $e->getMessage();
-    echo $dsn;
-    exit;
-}
+$db = new DB();
+$pdo = $db->pdo;
 
 //POSTデータ取得
 $posts = $_POST;
