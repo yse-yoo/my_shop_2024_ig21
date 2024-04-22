deb8f30ff0228f8324f1a2f65c6c85551ebf1b49
yse-yoo <youhei_yoshikawa@yse-net.com>
2024-04-22 10:17:03 +0900

    new project

---
+++ regist/input.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Shop</title>
+</head>
+
+<body>
+    <h2>会員登録</h2>
+    <div>
+        <label for="">名前</label>
+        <input type="text" name="name">
+    </div>
+
+    <div>
+        <label for="">Email</label>
+        <input type="email" name="email">
+    </div>
+
+    <div>
+        <label for="">パスワード</label>
+        <input type="password" name="password">
+    </div>
+
+    <div>
+        <button>次へ</button>
+    </div>
+</body>
+
+</html>
