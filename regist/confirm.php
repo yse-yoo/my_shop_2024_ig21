16fab03f1d53f651657cce3409b4f4d61b9284cf
yse-yoo <youhei_yoshikawa@yse-net.com>
2024-04-22 10:28:39 +0900

    update

---
+++ regist/confirm.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Shop</title>
+</head>
+
+<body>
+    <h2>会員登録確認</h2>
+    <!-- form request confirm.php にデータをPOST送信-->
+    <form action="" method="post">
+        <div>
+            <label for="">名前</label>
+        </div>
+
+        <div>
+            <label for="">Email</label>
+        </div>
+
+        <div>
+            <label for="">パスワード</label>
+        </div>
+
+        <div>
+            <button>登録</button>
+            <a href="input.php">修正</a>
+        </div>
+    </form>
+</body>
+
+</html>
