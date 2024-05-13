785be801d3bc3b4affe1d9f6c3fa53771ccc48e8
yse-yoo <youhei_yoshikawa@yse-net.com>
2024-05-13 14:18:50 +0900

    update

--- admin_item/edit.php
+++ admin_item/edit.php
@@ -37,32 +37,34 @@ $item = $stmt->fetch(PDO::FETCH_ASSOC);
 <body>
     <div class="container">
         <h2 class="h2">商品編集</h2>
-        <form action="update.php" method="post">
-            <input type="hidden" name="id" value="<?= $item['id'] ?>">
+        <div>
+            <form action="update.php" method="post">
+                <input type="hidden" name="id" value="<?= $item['id'] ?>">
 
-            <div class="mb-3 form-floating">
-                <input id="code" type="text" class="form-control" name="code" value="<?= @$item['code'] ?>">
-                <label for="code">商品コード</label>
-                <p class="text-danger pt-2"><?= @$errors['code'] ?></p>
-            </div>
-            <div class="mb-3 form-floating">
-                <input id="name" type="text" class="form-control" name="name" value="<?= @$item['name'] ?>">
-                <label for="name">商品名</label>
-                <p class="text-danger pt-2"><?= @$errors['name'] ?></p>
-            </div>
-            <div class="mb-3 form-floating">
-                <input id="price" type="text" class="form-control" name="price" value="<?= @$item['price'] ?>">
-                <label for="price">価格</label>
-                <p class="text-danger pt-2"><?= @$errors['price'] ?></p>
-            </div>
-            <div class="mb-3 form-floating">
-                <input id="stock" type="text" class="form-control" name="stock" value="<?= @$item['stock'] ?>">
-                <label for="stock">在庫</label>
-                <p class="text-danger pt-2"><?= @$errors['stock'] ?></p>
-            </div>
-            <button class="btn btn-primary">更新</button>
-            <a class="btn btn-outline-primary" href="./">戻る</a>
-        </form>
+                <div class="mb-3 form-floating">
+                    <input id="code" type="text" class="form-control" name="code" value="<?= @$item['code'] ?>">
+                    <label for="code">商品コード</label>
+                    <p class="text-danger pt-2"><?= @$errors['code'] ?></p>
+                </div>
+                <div class="mb-3 form-floating">
+                    <input id="name" type="text" class="form-control" name="name" value="<?= @$item['name'] ?>">
+                    <label for="name">商品名</label>
+                    <p class="text-danger pt-2"><?= @$errors['name'] ?></p>
+                </div>
+                <div class="mb-3 form-floating">
+                    <input id="price" type="text" class="form-control" name="price" value="<?= @$item['price'] ?>">
+                    <label for="price">価格</label>
+                    <p class="text-danger pt-2"><?= @$errors['price'] ?></p>
+                </div>
+                <div class="mb-3 form-floating">
+                    <input id="stock" type="text" class="form-control" name="stock" value="<?= @$item['stock'] ?>">
+                    <label for="stock">在庫</label>
+                    <p class="text-danger pt-2"><?= @$errors['stock'] ?></p>
+                </div>
+                <button class="btn btn-primary">更新</button>
+                <a class="btn btn-outline-primary" href="./">戻る</a>
+            </form>
+        </div>
 
         <div class="mt-3">
             <form action="delete.php" method="post">
