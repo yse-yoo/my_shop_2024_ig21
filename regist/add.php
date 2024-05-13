a2d5211653d180a91720744f072890eb9ca92a14
yse-yoo <youhei_yoshikawa@yse-net.com>
2024-05-13 10:22:50 +0900

    update

--- regist/add.php
+++ regist/add.php
@@ -1,4 +1,7 @@
 <?php 
+// env.php を読み込み
+require_once '../env.php';
+
 // lib/DB.php を読み込み
 require_once '../lib/DB.php';
 
@@ -15,7 +18,19 @@ session_regenerate_id(true);
 // セッションデータ取得
 $regist = $_SESSION['my_shop']['regist'];
 
+// データベースに接続
+$db = new DB();
+
 // TODO: データベースに登録
+// users テーブルにレコードを挿入するSQL
+// CRUD:
+// Create: INSERT INTO
+// Read: SLELECT
+// Update: UPDATE
+// Delete: DELETE
+$sql = "INSERT INTO users (name, email, passoword)
+        VALUES (:name, :email, :password);
+        ";
 
 // TODO: 予期せぬエラーの場合は、入力画面にリダイレクト
 // TODO: 成功の場合は、完了画面にリダイレクト
