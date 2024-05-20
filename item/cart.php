f4b6a0530a626dc168a3d37a7b30d4d86fd72979
yse-yoo <youhei_yoshikawa@yse-net.com>
2024-05-20 10:51:55 +0900

    update

--- item/cart.php
+++ item/cart.php
@@ -5,12 +5,18 @@ require_once '../env.php';
 // lib/DB.php を読み込み
 require_once '../lib/DB.php';
 
+// セッション開始
+session_start();
+session_regenerate_id(true);
+
 // 商品IDの取得
 $item_id = $_GET['item_id'];
 // var_dump($item_id);
 
 // カートに追加
 addCart($item_id);
+
+var_dump($_SESSION['my_shop']['cart_items']);
 
 function addCart($item_id)
 {
@@ -22,4 +28,9 @@ function addCart($item_id)
     $stmt = $db->pdo->prepare($sql);
     $stmt->execute(['id' => $item_id]);
     $item = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    // 商品があればセッションに登録
+    if ($item) {
+        $_SESSION['my_shop']['cart_items'][$item_id] = $item;
+    }
 }
