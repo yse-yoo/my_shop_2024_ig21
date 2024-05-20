e08693258550232bd8cee6277e14d35af8ace02a
yse-yoo <youhei_yoshikawa@yse-net.com>
2024-05-20 10:47:34 +0900

    update

--- item/cart.php
+++ item/cart.php
@@ -1,23 +1,25 @@
-<?php 
+<?php
 // env.php を読み込み
 require_once '../env.php';
 
 // lib/DB.php を読み込み
 require_once '../lib/DB.php';
 
-// DBに接続する
-$db = new DB();
-
 // 商品IDの取得
 $item_id = $_GET['item_id'];
 // var_dump($item_id);
 
-// DBから items.id を使って商品の取得
-$sql = "SELECT * FROM items WHERE id = :id;";
-$stmt = $db->pdo->prepare($sql);
-$stmt->execute(['id' => $item_id]);
-$item = $stmt->fetch(PDO::FETCH_ASSOC);
+// カートに追加
+addCart($item_id);
 
-var_dump($item);
+function addCart($item_id)
+{
+    // DBに接続する
+    $db = new DB();
 
-?>
+    // DBから items.id を使って商品の取得
+    $sql = "SELECT * FROM items WHERE id = :id;";
+    $stmt = $db->pdo->prepare($sql);
+    $stmt->execute(['id' => $item_id]);
+    $item = $stmt->fetch(PDO::FETCH_ASSOC);
+}
