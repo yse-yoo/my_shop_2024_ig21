5b28a17b1efd606dd83880da8c43309eeaa2f0d0
yse-yoo <youhei_yoshikawa@yse-net.com>
2024-05-20 10:33:01 +0900

    update

--- item/cart.php
+++ item/cart.php
@@ -1,7 +1,18 @@
 <?php 
+// env.php を読み込み
+require_once '../env.php';
+
+// lib/DB.php を読み込み
+require_once '../lib/DB.php';
+
+// DBに接続する
+$db = new DB();
 
 // 商品IDの取得
 $item_id = $_GET['item_id'];
 // var_dump($item_id);
+
+
+// DBから items.id を使って商品の取得
 
 ?>
